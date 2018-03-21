caf78860dbd70596e539763822038e38f2020a5d
shunyao.zeng <shunyao.zeng@mail.funi365.com>
2018-03-21 22:20:16 +0800

    day-06 ok2

--- www/config_default.py
+++ www/config_default.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # config_default.py
+# 配置文件
+'''
+Default configurations.
+'''
+
+__author__ = 'Michael Liao'
 
 configs = {
+    'debug': True,
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www-data',
-        'password': 'www-data',
-        'database': 'awesome'
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
     },
     'session': {
-        'secret': 'AwEsOmE'
+        'secret': 'Awesome'
     }
 }
--- www/config_override.py
+++ www/config_override.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # config_override.py
+# 覆盖某些默认设置
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
 
 configs = {
     'db': {
-        'host': '192.168.0.100'
+        'host': '127.0.0.1'
     }
 }
