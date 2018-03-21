c287c518a8f0ebcc662fa3be048088da079796c2
shunyao.zeng <shunyao.zeng@mail.funi365.com>
2018-03-21 22:09:36 +0800

    day-06 ok

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# config_override.py
+
+configs = {
+    'db': {
+        'host': '192.168.0.100'
+    }
+}
